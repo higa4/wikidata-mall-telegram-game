8d89de8823e8bd4120f40999133290d1351401dd
Edgar Toll <edjopato@gmail.com>
2019-07-02 01:51:02 +0200

    feat: use shorter number format

--- source/lib/interface/formatted-strings.ts
+++ source/lib/interface/formatted-strings.ts
@@ -33,5 +33,26 @@ export function infoHeader(wdr: WikidataEntityReader, options: InfoHeaderOptions
 }
 
 export function labeledNumber(wdr: WikidataEntityReader, num: number, currency: string): string {
-	return `${wdr.label()}: ${num}${currency}`
+	return `${wdr.label()}: ${formattedInt(num)}${currency}`
+}
+
+const LETTERS = ['', 'k', 'M', 'G', 'T', 'P', 'E']
+
+export function formattedInt(num: number): string {
+	const exp = Math.floor(Math.log10(Math.abs(num)))
+	const sciExp = Math.floor(exp / 3) * 3
+	const sciNum = 10 ** sciExp
+
+	if (sciNum === 0) {
+		return '0'
+	}
+
+	const relevantNumPart = num / sciNum
+
+	const fractionDigits = sciExp > 2 ? 2 : 0
+	const numberString = relevantNumPart.toFixed(fractionDigits)
+
+	const letterString = LETTERS[sciExp / 3]
+
+	return numberString + letterString
 }
