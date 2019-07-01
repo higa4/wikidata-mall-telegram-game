064df74129d5046e0f8c0e743a3242d3a56ed033
Edgar Toll <edjopato@gmail.com>
2019-07-01 22:57:44 +0200

    fix(people): lower talents gausian norm
    it can be increased later but I dont want to lower something when the 
    game is running

--- source/lib/session-math/applicants.ts
+++ source/lib/session-math/applicants.ts
@@ -59,7 +59,7 @@ function addWaitingApplicants(session: Session, now: number): void {
 }
 
 const MINIMAL_TALENT = 0.001
-const talentDistribution = gaussian(1.1, 0.1)
+const talentDistribution = gaussian(1, 0.1)
 /* DEBUG
 console.log('talentDistribution propability', '<0  :', talentDistribution.cdf(MINIMAL_TALENT))
 console.log('talentDistribution propability', '<0.2:', talentDistribution.cdf(0.2))
@@ -67,6 +67,7 @@ console.log('talentDistribution propability', '>1  :', 1 - talentDistribution.cd
 console.log('talentDistribution propability', '>1.5:', 1 - talentDistribution.cdf(1.5))
 console.log('talentDistribution propability', '>1.8:', 1 - talentDistribution.cdf(1.8))
 console.log('talentDistribution propability', '>2  :', 1 - talentDistribution.cdf(2))
+console.log('talentDistribution propability', '>2.5:', 1 - talentDistribution.cdf(2.5))
 /**/
 
 function randomTalents(): Talents {
