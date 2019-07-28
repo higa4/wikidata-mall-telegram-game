28dfcdfcd779e36f82ded1632bce4a43fad2e240
Edgar Toll <edjopato@gmail.com>
2019-07-28 13:51:48 +0200

    feat(fibonacci): add fillArray and cached

--- source/lib/math/fibonacci.ts
+++ source/lib/math/fibonacci.ts
@@ -6,3 +6,23 @@ export function stateful(initialValue = 1): (next: number) => number {
 		return result
 	}
 }
+
+const cache = [0, 1]
+export function cached(i: number): number {
+	fillArray(cache, i)
+	return cache[i]
+}
+
+export function fillArray(numbers: number[], untilIndex: number): void {
+	if (numbers.length < 2) {
+		throw new Error('number array is too short for fibonaci calculation')
+	}
+
+	if (!Number.isFinite(untilIndex)) {
+		throw new TypeError('only finite fibonacci number indicies are supported')
+	}
+
+	while (numbers.length <= untilIndex) {
+		numbers.push(numbers[numbers.length - 2] + numbers[numbers.length - 1])
+	}
+}
--- test/math/fibonacci.ts
+++ test/math/fibonacci.ts
@@ -1,6 +1,6 @@
 import test from 'ava'
 
-import {stateful} from '../../source/lib/math/fibonacci'
+import {cached, fillArray, stateful} from '../../source/lib/math/fibonacci'
 
 test('stateful example', t => {
 	const func = stateful()
@@ -9,4 +9,24 @@ test('stateful example', t => {
 	t.is(func(3), 5)
 	t.is(func(5), 8)
 	t.is(func(8), 13)
+})
+
+test('fillArray example', t => {
+	const arr = [0, 1]
+	fillArray(arr, 5)
+	t.deepEqual(arr, [0, 1, 1, 2, 3, 5])
+})
+
+test('fillArray does not work to infinity', t => {
+	const arr = [0, 1]
+	t.throws(() => fillArray(arr, Infinity), /finite/)
+})
+
+test('fillArray does need start numbers', t => {
+	t.throws(() => fillArray([], 5), /array.+too short/)
+})
+
+test('cached works', t => {
+	t.is(cached(1), 1)
+	t.is(cached(5), 5)
 })
