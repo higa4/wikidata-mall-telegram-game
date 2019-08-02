fd71b56199aa1936420b4561a1367ab3ebaed1a8
Edgar Toll <edjopato@gmail.com>
2019-08-03 00:00:36 +0200

    test(formatted-time): humanReadableTimestamp

--- test/interface/formatted-time.ts
+++ test/interface/formatted-time.ts
@@ -1,6 +1,6 @@
 import test from 'ava'
 
-import {countdownHourMinute} from '../../source/lib/interface/formatted-time'
+import {countdownHourMinute, humanReadableTimestamp} from '../../source/lib/interface/formatted-time'
 
 test('countdownHourMinute examples', t => {
 	t.is(countdownHourMinute(42), '0:00')
@@ -9,4 +9,10 @@ test('countdownHourMinute examples', t => {
 	t.is(countdownHourMinute(60 * 60), '1:00')
 	t.is(countdownHourMinute(65 * 60), '1:05')
 	t.is(countdownHourMinute(600 * 60 * 60), '600:00')
+})
+
+test('humanReadableTimestamp', t => {
+	t.is(humanReadableTimestamp(1564782891, 'de'), '2. Aug. 2019, 21:54 UTC')
+	t.is(humanReadableTimestamp(1564782891, 'en-US'), 'Aug 2, 2019, 9:54 PM UTC')
+	t.is(humanReadableTimestamp(1564782891, 'en-GB'), '2 Aug 2019, 21:54 UTC')
 })
