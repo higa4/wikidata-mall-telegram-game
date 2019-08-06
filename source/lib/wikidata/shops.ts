359b88fe62e4459c87e0f15e0b8264569e8685ff
Edgar Toll <edjopato@gmail.com>
2019-08-06 17:36:03 +0200

    feat(shops): require only 5 products per shop
    with the skill in place now only 5 products can be fun too
    also it allows for more possible shops

--- source/lib/wikidata/shops.ts
+++ source/lib/wikidata/shops.ts
@@ -34,7 +34,7 @@ FILTER(EXISTS { ?item wdt:P18 ?image. })
 FILTER(LANG(?label) = "en")
 }
 GROUP BY ?class
-HAVING ((COUNT(?item)) >= 6 )`
+HAVING ((COUNT(?item)) >= 5 )`
 }
 
 function productsQuery(shopType: string): string {
