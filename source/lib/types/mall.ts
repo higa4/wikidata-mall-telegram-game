a6b9cbf74c5464fd8c2f9974144f0e965297b251
Edgar Toll <edjopato@gmail.com>
2019-09-10 16:23:17 +0200

    refactor(mall): use type UserId
    improves readability

--- source/lib/types/mall.ts
+++ source/lib/types/mall.ts
@@ -1,9 +1,11 @@
 import {Chat} from 'telegram-typings'
 
+type UserId = number
+
 export interface Mall {
 	// TODO: add attraction
 	// attraction?: string;
-	member: number[];
+	member: UserId[];
 	money: number;
 	chat: Chat;
 }
